d9117942c8bcf23fa66c52f4907d3d9a48015107
iaguirre19 <iran.agui1993@gmail.com>
2022-06-11 01:05:25 -0500

    sidebar nav it's done

--- navbar.js
+++ navbar.js
@@ -1,11 +1,13 @@
 let btn = document.querySelector("#btn");
 let sidebar = document.querySelector(".sidebar");
 let searchBtn = document.querySelector(".bx-search");
+let icono = document.querySelector(".bxl-c-plus-plus");
+let btn1 = document.querySelector("bx-menu");
+
 
 btn.onclick = function(){
-    sidebar.classList.toggle("active");
+    sidebar.classList.toggle("active"); /**Nos permite agregar una clase cuando se agrega una clase**/
 }
-
 searchBtn.onclick = function(){
     sidebar.classList.toggle("active");
 }
